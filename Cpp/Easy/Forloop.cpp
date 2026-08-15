e348f917ec3cad740b7754ca0400b0e93cad2a38
José Gomes <jgsnto.sd@gmail.com>
2026-08-15 21:02:11 +0200

    Cpp-easy: For loops

---
+++ Cpp/Easy/Forloop.cpp
@@ -0,0 +1,48 @@
+/*
+A for loop is a programming language statement which allows code to be repeatedly executed.
+
+The syntax is
+
+for ( <expression_1> ; <expression_2> ; <expression_3> )
+    <statement>
+
+expression_1 is used for intializing variables which are generally used for controlling the terminating flag for the loop.
+expression_2 is used to check for the terminating condition. If this evaluates to false, then the loop is terminated.
+expression_3 is generally used to update the flags/variables.
+
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    //cin >> a >> b;
+    scanf("%d %d", &a, &b);
+    for (int i=a; i <= b; i++){
+        if (i==1){
+        cout << "one\n";
+        } else if(i==2){
+            cout << "two\n";
+        } else if(i==3){
+            cout << "three\n";
+        } else if(i==4){
+            cout << "four\n";
+        } else if(i==5){
+            cout << "five\n";
+        } else if(i==6){
+            cout << "six\n";
+        } else if(i==7){
+            cout << "seven\n";
+        } else if(i==8){
+            cout << "eight\n";
+        } else if(i==9) {
+            cout << "nine\n";
+        } else if(i>9 && i%2 == 0) {
+            cout << "even\n";
+        } else {
+            cout << "odd\n";
+        }
+    }
+    return 0;
+}
